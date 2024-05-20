8bd16becea6ffac56291044e770b23911601cdcd
prgrmz07 <prgrmz07@163.com>
2024-05-20 22:57:49 +0800

    测试材料test_main.c

---
+++ test_in/test_main.c
@@ -0,0 +1,24 @@
+#define INT_T int
+typedef double DOUBLE_typedef;
+
+struct Point{
+    float x;
+    double y;
+};
+
+void voidDemo3(){
+    int k=0;
+    k++;
+
+}
+int main(int argc, char** argv){
+    struct Point pnt1;
+    struct Point pnt2;
+    {
+        struct Point * ptr1=&pnt1;
+        struct Point * ptr2=&pnt2;
+        struct Point pnt3;
+    }
+
+  return 0;
+ }
