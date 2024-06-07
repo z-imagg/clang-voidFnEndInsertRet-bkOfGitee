3df2c928364f546f6255dd80d28d04fabb959e65
prgrmz07 <prgrmz07@163.com>
2024-06-07 19:44:49 +0800

    sleuthkit-4.12.1 测试材料

--- test_in/test_main.cpp
+++ test_in/test_main.cpp
@@ -1,87 +1,23 @@
-#define CASE_SAME(x) case x:
-#define INT_T int
-typedef double DOUBLE_typedef;
-
-struct Point{
-    float x;
-    double y;
-};
-class UserEntity{};
-class ScoreRelation{
-protected:
-  float calcScoreSum(int cnt, char t);
-};
-float ScoreRelation::calcScoreSum(int cnt, char t){
-    int dummy;
-    return 3.9*cnt+t=='z'?5:-5;
-}
-
-class MyClass {
+//来源: https://gitee.com/repok/sleuthkit-4.12.1/blob/master/tsk/fs/ntfs_dent.cpp
+class NTFS_PAR_MAP  {
 public:
-    static int ZERO;
-public:
-    double m_d1;
-    int m_n2;
 
-    MyClass( ){}
-
-    MyClass(float f1, int n1)
-    :m_d1(f1),m_n2(n1)
-    {
-        char c3=1+n1;
-    }
-
-    ~MyClass(){
-        int x,y,z;
-        UserEntity userEntity;
-
-        Point point0;
-        auto fn_point = [](const Point& point) {
-            if(point.x>point.y)
-                return point.x+point.y;
+        /**
+         * Test if there are any children for this directory at a given sequence.
+         * @param seq Sequence to test.
+         * @returns true if children exist
+         */
+        bool exists (int seq) {
+            if ((seq) > 0) 
+                return true;
             else
-                return 0.1;
-        };
+                return false;
+            //这里是不应该插入return语句的
+            //错误的在这里插入return语句的原因是, 判断'如果是当前块语句的父节点' 写反了, 没有正确的容纳c++普通函数， 只容纳了普通函数
+        }
+ 
+ };
 
-        fn_point(point0);
-
-
-        auto fn_pnt = [](const Point& pnt) {
-            return pnt.x+pnt.y;
-        };
-        fn_pnt(point0);
-        
-    }
-
-  void voidDemo(int cnt, short chr){
-  }
-
-    void voidDemo2(UserEntity userEntity){
-      if(true){
-          return;
-      }
-    }
-
-
-
-};
-
-int MyClass::ZERO=0;
-
-void voidDemo3(){
-    int k=0;
-    k++;
-    
-}
-int main(int argc, char** argv){
-    MyClass varMyClass;
-    static Point pnt1;
-    struct Point pnt2;
-    {
-        struct Point * ptr1=&pnt1;
-        struct Point * ptr2=&pnt2;
-        struct Point pnt3;
-    }
-
+ int main(int argc, char** argv){
   return 0;
  }
